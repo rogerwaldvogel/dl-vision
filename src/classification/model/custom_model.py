71927847b85083df62d2065d6e48d3102af38e8b
roger <climberwoodi@gmx.ch>
2023-02-09 15:41:04 +0100

    added a model for custom cnn networks.

---
+++ src/classification/model/custom_model.py
@@ -0,0 +1,35 @@
+from .ModelTexture import ModelTexture
+import tensorflow as tf
+
+
+class CustomModel(ModelTexture):
+    def __init__(self, base_model, model_name):
+        super().__init__(base_model, model_name)
+
+    def _get_model(self, base_model):
+        model = tf.keras.Sequential([
+            tf.keras.layers.InputLayer((150, 150, 3)),
+            tf.keras.layers.Conv2D(32, 3, activation=tf.keras.activations.relu),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.MaxPool2D(),
+            tf.keras.layers.Conv2D(64, 3, activation=tf.keras.activations.relu),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.MaxPool2D(),
+            tf.keras.layers.Conv2D(128, 3, activation=tf.keras.activations.relu),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.AveragePooling2D(),
+            tf.keras.layers.Conv2D(256, 3, activation=tf.keras.activations.relu),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.MaxPool2D(),
+            tf.keras.layers.Conv2D(512, 3, activation=tf.keras.activations.relu),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.MaxPool2D(),
+            tf.keras.layers.Flatten(),
+            # tf.keras.layers.Dropout(0.4),
+            tf.keras.layers.Dense(512, activation=tf.keras.activations.relu),
+            tf.keras.layers.Dropout(0.4),
+            tf.keras.layers.Dense(512, activation=tf.keras.activations.relu),
+            tf.keras.layers.Dropout(0.4),
+            tf.keras.layers.Dense(8, activation=tf.keras.activations.softmax)
+        ], name='custom_model')
+        return model
