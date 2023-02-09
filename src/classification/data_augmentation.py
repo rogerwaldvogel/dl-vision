a42315433ae143723d6faaea614f1024d295a983
roger <climberwoodi@gmx.ch>
2023-02-09 15:41:20 +0100

    simpler data augmentation

--- src/classification/data_augmentation.py
+++ src/classification/data_augmentation.py
@@ -4,13 +4,10 @@ import numpy as np
 
 def add_augmented_data(x, y, batch_size=32, number_of_batches=10):
     aug = ImageDataGenerator(
-        rotation_range=20,
-        zoom_range=0.15,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.15,
-        horizontal_flip=True,
-        fill_mode="nearest")
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.1)
 
     data_generator = aug.flow(x, y, batch_size=batch_size)
     for i in range(0, number_of_batches):
