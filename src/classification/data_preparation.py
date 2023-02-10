5cb74dacd7a9f48b3ad5afca70c759509d0254c8
roger <climberwoodi@gmx.ch>
2023-02-10 16:46:00 +0100

    removed crappy shuffle function.

--- src/classification/data_preparation.py
+++ src/classification/data_preparation.py
@@ -1,11 +1,9 @@
 import os
 import tensorflow as tf
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.utils import to_categorical
 import re
-import random
 import numpy as np
 
 
@@ -22,14 +20,10 @@ def get_data(path):
             image = load_img(file_path, target_size=(150, 150))
             image = img_to_array(image)
             image = tf.cast(image, tf.float32) / 255.0
-            # image = preprocess_input(image)
             data.append(image)
             labels.append(get_label(folder))
 
     labels = to_categorical(labels, num_classes=8)
-    combined = list(zip(data, labels))
-    random.shuffle(combined)
-    data[:], labels[:] = zip(*combined)
     data = np.array(data, dtype="float32")
     labels = np.array(labels)
     return data, labels
