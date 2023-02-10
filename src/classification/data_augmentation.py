931850d06b6784c7b56cdd27854370ef87008ebf
roger <climberwoodi@gmx.ch>
2023-02-10 16:45:39 +0100

    set seed.

--- src/classification/data_augmentation.py
+++ src/classification/data_augmentation.py
@@ -9,7 +9,7 @@ def add_augmented_data(x, y, batch_size=32, number_of_batches=10):
         height_shift_range=0.1,
         zoom_range=0.1)
 
-    data_generator = aug.flow(x, y, batch_size=batch_size)
+    data_generator = aug.flow(x, y, batch_size=batch_size, seed=42)
     for i in range(0, number_of_batches):
         x_batch, y_batch = data_generator.next()
         x = np.concatenate((x, x_batch))
