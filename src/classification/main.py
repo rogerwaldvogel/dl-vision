a92b8c31d43b2787a756a8745de82a6cf1b3c87d
roger <climberwoodi@gmx.ch>
2023-02-10 16:44:26 +0100

    using mobilenetv2. optimized hyperparameter

--- src/classification/main.py
+++ src/classification/main.py
@@ -1,62 +1,45 @@
 import data_preparation
 import data_augmentation
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from model.ModelTexture import ModelTexture
 from model.custom_model import CustomModel
 from sklearn.model_selection import train_test_split
 
 from tensorflow.keras.layers import Input
 from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.applications import Xception
-from tensorflow.keras.applications import InceptionResNetV2
 from tensorflow.keras.applications import ResNet50V2
 
-
 if __name__ == '__main__':
-    INIT_LR = 1e-3  # learning rate
-    EPOCHS = 50  # number of epochs
-    BS = 64  # batch size
+    INIT_LR = 1e-4  # learning rate
+    EPOCHS = 20  # number of epochs
+    BS = 128  # batch size
 
     data, labels = data_preparation.get_data('data/Kather_texture_2016_image_tiles_5000')
-    (train_x, test_x, train_y, test_y) = train_test_split(data, labels,
-                                                          test_size=0.20, stratify=labels, random_state=42)
-    train_x, train_y = data_augmentation.add_augmented_data(train_x, train_y,
-                                                            batch_size=BS,
-                                                            number_of_batches=100)
-    aug_per_epoch = ImageDataGenerator(
-        rotation_range=20,
-        zoom_range=0.15,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.15,
-        horizontal_flip=True,
-        fill_mode="nearest")
 
-    # base_model = MobileNetV2(weights="imagenet", include_top=False, input_tensor=Input(shape=(150, 150, 3)))
-    # model = ModelTexture(base_model, "mobilenetv2")
-    # model.configure_model(learning_rate=INIT_LR, weight_decay=INIT_LR / EPOCHS)
-    # model.train(aug_per_epoch, train_x, train_y, test_x, test_y, epochs=EPOCHS, batch_size=BS)
+    data, labels = data_augmentation.add_augmented_data(data, labels,
+                                                        batch_size=BS,
+                                                        number_of_batches=150)
 
-    # base_model = Xception(weights="imagenet", include_top=False, input_tensor=Input(shape=(150, 150, 3)))
-    # model = ModelTexture(base_model, "xception")
-    # model.configure_model(learning_rate=INIT_LR, weight_decay=INIT_LR / EPOCHS)
-    # model.train(aug_per_epoch, train_x, train_y, test_x, test_y, epochs=EPOCHS, batch_size=BS)
+    (train_x, val_x, train_y, val_y) = train_test_split(data, labels,
+                                                        test_size=0.20, stratify=labels, random_state=42)
+    (train_x, test_x, train_y, test_y) = train_test_split(train_x, train_y,
+                                                          test_size=0.20, stratify=train_y, random_state=42)
 
-    # base_model = InceptionResNetV2(weights="imagenet", include_top=False, input_tensor=Input(shape=(150, 150, 3)))
-    # model = ModelTexture(base_model, "inceptionresnetv2")
-    # model.configure_model(learning_rate=INIT_LR, weight_decay=INIT_LR / EPOCHS)
-    # model.train(aug_per_epoch, train_x, train_y, test_x, test_y, epochs=EPOCHS, batch_size=BS)
+    base_model = MobileNetV2(weights="imagenet", include_top=False,
+                             input_tensor=Input(shape=(150, 150, 3)),
+                             input_shape=(150, 150, 3))
+    model = ModelTexture(base_model, "mobilenetv2")
+    model.configure_model(learning_rate=INIT_LR)
+    model.train(train_x, train_y, val_x, val_y, epochs=EPOCHS, batch_size=BS)
 
-    # base_model = InceptionResNetV2(weights="imagenet", include_top=False, input_tensor=Input(shape=(150, 150, 3)))
-    # model = ModelTexture(base_model, "inceptionresnetv2")
-    # model.configure_model(learning_rate=INIT_LR, weight_decay=INIT_LR / EPOCHS)
-    # model.train(aug_per_epoch, train_x, train_y, test_x, test_y, epochs=EPOCHS, batch_size=BS)
-
-    # base_model = ResNet50V2(weights="imagenet", include_top=False, input_tensor=Input(shape=(150, 150, 3)))
+    # base_model = ResNet50V2(weights="imagenet", include_top=False,
+    #                         input_tensor=Input(shape=(150, 150, 3)),
+    #                         input_shape=(150, 150, 3))
     # model = ModelTexture(base_model, "resnet50v2")
-    # model.configure_model(learning_rate=INIT_LR, weight_decay=INIT_LR / EPOCHS)
+    # model.configure_model(learning_rate=INIT_LR)
     # model.train(aug_per_epoch, train_x, train_y, test_x, test_y, epochs=EPOCHS, batch_size=BS)
 
-    model = CustomModel(None, "custom_model")
-    model.configure_model(learning_rate=INIT_LR, weight_decay=None)
-    model.train(aug_per_epoch, train_x, train_y, test_x, test_y, epochs=EPOCHS, batch_size=BS)
+    # model = CustomModel(None, "custom_model")
+    # model.configure_model(learning_rate=INIT_LR)
+    # model.train(aug_per_epoch, train_x, train_y, test_x, test_y, epochs=EPOCHS, batch_size=BS)
+
+    model.evaluate(test_x, test_y)
