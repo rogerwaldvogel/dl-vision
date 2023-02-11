d73ec4caae8504e7ef89b4439e2416c60c72af9f
roger <climberwoodi@gmx.ch>
2023-02-11 12:04:53 +0100

    Using kernel regularizer and fewer neurons to avoid overfitting.

--- src/classification/model/ModelTexture.py
+++ src/classification/model/ModelTexture.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
@@ -11,13 +12,13 @@ class ModelTexture:
         self.model_name = model_name
 
     def _get_model(self, base_model):
-
         head_model = base_model.output
         head_model = Flatten(name="flatten")(head_model)
-        head_model = Dense(128, activation="relu",
-                           activity_regularizer=tf.keras.regularizers.L2(0.001))(head_model)
+        head_model = Dense(64, activation="relu",
+                           kernel_regularizer=tf.keras.regularizers.L2(0.001))(head_model)
         head_model = Dropout(0.2)(head_model)
-        head_model = Dense(64, activation="relu")(head_model)
+        head_model = Dense(64, activation="relu",
+                           kernel_regularizer=tf.keras.regularizers.L2(0.001))(head_model)
         head_model = Dense(8, activation="softmax")(head_model)
         model = Model(inputs=base_model.input, outputs=head_model)
         # We don't want to train any layer of the base model
@@ -29,7 +30,7 @@ class ModelTexture:
         lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
             learning_rate,
             decay_steps=100,
-            decay_rate=0.99,
+            decay_rate=0.98,
             staircase=True)
 
         opt = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
@@ -37,19 +38,20 @@ class ModelTexture:
                            metrics=["accuracy", "Precision", "Recall"])
         print(self.model.summary())
 
-    def train(self, train_x, train_y, test_x, test_y, epochs=30, batch_size=32):
+    def train(self, train_x, train_y, val_x, val_y, epochs=30, batch_size=32):
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs")
         early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=4)
-        self.model.fit(
+        history = self.model.fit(
             x=train_x,
             y=train_y,
             steps_per_epoch=len(train_x) // batch_size,
-            validation_data=(test_x, test_y),
-            validation_steps=len(test_x) // batch_size,
+            validation_data=(val_x, val_y),
+            validation_steps=len(val_x) // batch_size,
             epochs=epochs,
             callbacks=[tensorboard_callback, early_stopping_callback])
 
         self.model.save(f"{self.model_name}.h", save_format="h5")
+        np.save(f'{self.model_name}.npy', history.history)
 
     def evaluate(self, test_x, test_y):
         print("Evaluating model...")
